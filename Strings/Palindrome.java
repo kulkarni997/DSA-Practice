d9df812b6adf715fc05424fb76f8ee2accd08bd3
Srushti Kulkarni <kulkarnisrushti101@gmail.com>
2026-02-16 18:16:37 +0530

    strings

---
+++ Strings/Palindrome.java
@@ -0,0 +1,20 @@
+package Strings;
+
+public class Palindrome{
+
+    public static boolean ispalindrome(String str){
+        int n = str.length();
+
+        for(int i=0; i<str.length()/2; i++){
+            
+            if(str.charAt(i) != str.charAt(n-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String str;
+        System.out.println(ispalindrome("hello"));
+    }
+}
